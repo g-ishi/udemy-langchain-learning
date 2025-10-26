dd8b4729bd70ac41da5fbb112613fde812969f43
g-ishi <al14006@shibaura-it.ac.jp>
2025-10-26 16:04:48 +0900

    finish: section18 mcp basics

---
+++ section18-mcp-server-and-client/main.py
@@ -0,0 +1,40 @@
+import asyncio
+import os
+
+from dotenv import load_dotenv
+from langchain_core.messages import HumanMessage
+from langchain_mcp_adapters.tools import load_mcp_tools
+from langchain_openai import ChatOpenAI
+from langgraph.prebuilt import create_react_agent
+from mcp import ClientSession, StdioServerParameters
+from mcp.client.stdio import stdio_client
+
+load_dotenv()
+
+llm = ChatOpenAI(model="gpt-4.1-mini")
+
+stdio_server_params = StdioServerParameters(
+    command="python",
+    args=[
+        "/Users/gen/Work/work_personal/42.langchain-udemy/langchain-course/section18-mcp-server-and-client/servers/math_server.py"
+    ],
+)
+
+
+async def main():
+    async with stdio_client(stdio_server_params) as (read, write):
+        async with ClientSession(read_stream=read, write_stream=write) as session:
+            await session.initialize()
+            print("session initialized")
+            tools = await load_mcp_tools(session)
+
+            agent = create_react_agent(llm, tools)
+
+            result = await agent.ainvoke(
+                {"messages": [HumanMessage(content="What is 54 + 2 * 3?")]}
+            )
+            print(result["messages"][-1].content)
+
+
+if __name__ == "__main__":
+    asyncio.run(main())
